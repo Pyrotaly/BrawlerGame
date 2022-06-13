e22a29088d9693fd16842660c09aa3616f57c85d
Pyrotaly <102985554+Pyrotaly@users.noreply.github.com>
2022-06-13 15:28:39 -0400

    Fixed Attacking
    Allows the player to spam attack consistently and if they do not input an attack for a certain time, the FSM will return to idle state.

--- BrawlerPrototype/Assets/Game/Scripts/Player/States/Combat/BaseLight/Ground/LightAttackState.cs
+++ BrawlerPrototype/Assets/Game/Scripts/Player/States/Combat/BaseLight/Ground/LightAttackState.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class LightAttackState : GroundStates
 {
     public D_LightAttacks lightAttackData;
+    private float timeStamp;
 
     public LightAttackState(Player player, string animBoolName, D_LightAttacks lightAttackData) : base(player, animBoolName)
     {
@@ -24,12 +25,23 @@ public class LightAttackState : GroundStates
         base.Exit();
         player.CharacterSelected.NextLightAttack[0] = Time.time + player.CharacterSelected.CharacterLightCooldowns[0];
         player.AnimCombat.SetBool("Light", false);
+        timeStamp = 0;
     }
 
     public override void LogicUpdate()
     {
         base.LogicUpdate();
 
+        player.Core.Movement.SetVelocityX(xInput * 10);
+
+        if (!player.Input.Attack)
+        {
+            timeStamp += Time.deltaTime;
+            if (timeStamp >= 0.3)
+            {
+                stateMachine.ChangeState(player.IdleState);
+            }
+        }
     }
 
     public override void PhysicsUpdate()
@@ -40,11 +52,6 @@ public class LightAttackState : GroundStates
     public override void AnimationTrigger()
     {
         base.AnimationTrigger();
-
-        if (!player.Input.Attack) 
-        {
-            stateMachine.ChangeState(player.IdleState);
-        }
     }
 
     public override void AnimationFinishedTrigger()

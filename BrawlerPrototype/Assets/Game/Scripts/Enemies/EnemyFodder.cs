8e37a2980a3f4acdb50c75629a7711db5b7258c0
Pyrotaly <102985554+Pyrotaly@users.noreply.github.com>
2022-06-14 15:32:19 -0400

    Adding in Fodder Flee

--- BrawlerPrototype/Assets/Game/Scripts/Enemies/EnemyFodder.cs
+++ BrawlerPrototype/Assets/Game/Scripts/Enemies/EnemyFodder.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyFodder : MonoBehaviour
+public class EnemyFodder : BaseEnemy
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void AttackAnimationTrigger()
     {
-        
-    }
+        base.AttackAnimationTrigger();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
     }
 }
